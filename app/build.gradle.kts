3d194154d721dee0db531fc9e29d5877a943e6b5
Mystery0 <mystery0dyl520@gmail.com>
2026-06-01 17:27:49 +0800

    添加 Koin 和测试依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,8 +93,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.koin.android)
 
     testImplementation(libs.junit)
+    testImplementation(libs.androidx.test.core)
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
